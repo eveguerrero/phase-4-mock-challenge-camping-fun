6ad0cb7a8a4bde853019a3397dd0a2d7ce05d021
Eveline Guerrero <evelineguerrero23@gmail.com>
2022-04-12 13:36:18 -0400

    commit tree 7f8004d74ca8032c85be232110a01985818caea0

---
+++ app/controllers/campers_controller.rb
@@ -0,0 +1,27 @@
+class CampersController < ApplicationController
+
+    def index 
+render json: Camper.all,  status: :ok
+    end
+
+    def show 
+    camper = Camper.find(params[:id])
+    render json: camper, status: :ok, serializer: CamperActivitySerializer
+    rescue ActiveRecord::RecordNotFound
+        render json: {error: "Camper not found"}, status: :not_found
+    end
+
+
+    def create 
+        camper = Camper.create!(camper_params)
+        render json: camper, status: :created
+    rescue ActiveRecord::RecordInvalid => e
+        render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity
+    end
+
+    private 
+
+    def camper_params 
+params.permit(:name, :age)
+    end
+end
